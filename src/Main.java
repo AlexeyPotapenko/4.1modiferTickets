6786cc5de7876e0ef0fe24e582e252a244b43e4b
Alexey Potapenko <Alesha_potapenko@mail.ru>
2021-07-19 17:52:55 +0300

    Firstquest

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 5000;
+        int bonus_cost = 20;
+        int bonus_total = (ticket / bonus_cost);
+        System.out.println(bonus_total);
+
+    }
+}
