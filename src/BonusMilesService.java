8989a4601339f13f7912001d722d3cfbb71893bf
Alexey Potapenko <Alesha_potapenko@mail.ru>
2021-07-21 19:34:40 +0300

    completed

---
+++ src/BonusMilesService.java
@@ -0,0 +1,25 @@
+public class BonusMilesService {
+    public int calculate(){
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//public class Main {
+//    public static void main(String[] args) {
+//        int cost = 5000;
+//        int bonus_cost = 20;
+//        int bonus_total = (cost / bonus_cost);
+//        System.out.println(bonus_total);
+//
+//    }
+//}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 13450;
-        int add = 2700;
-        int bonus = 0;
-        balance = balance + add;
-        if (add > 1000) {
-            bonus = add / 100;
-            balance = balance + bonus;
-        }
-        System.out.println(balance);
-        System.out.println(bonus);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
